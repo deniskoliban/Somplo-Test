bd38f1a62bd4ee2b764c3293ef9ffde3000aec0e
Denis Koliban <deniskoliban@gmail.com>
2021-08-11 14:40:16 +0300

    align elements

--- src/app/app-modules/material.module.ts
+++ src/app/app-modules/material.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 
+
+const MaterialModules = [
+  MatCardModule
+]
 
 @NgModule({
   imports: [
     CommonModule,
+    MaterialModules
   ],
   exports: [
+    MaterialModules
   ]
 })
 export class MaterialModule { }
