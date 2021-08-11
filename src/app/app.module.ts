27084d160f38b5bad38e4caf5a718ba937f32708
Denis Koliban <deniskoliban@gmail.com>
2021-08-11 13:32:45 +0300

    create material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { CreateHtmlComponent } from './create-html/create-html.component';
 import { CreateFormComponent } from './create-html/create-form/create-form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
