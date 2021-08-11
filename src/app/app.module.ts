a144201ac89fad513d8d4be390f7c145e5ef2800
Denis Koliban <deniskoliban@gmail.com>
2021-08-11 15:13:14 +0300

    create cropper component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,22 @@ import { CreateFormComponent } from './create-html/create-form/create-form.compo
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './app-modules/material.module';
 import { PreviewComponent } from './create-html/preview/preview.component';
+import { ImageCropperModule } from 'ngx-image-cropper';
+import { CropperComponent } from './cropper/cropper.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CreateHtmlComponent,
     CreateFormComponent,
-    PreviewComponent
+    PreviewComponent,
+    CropperComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    ImageCropperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
