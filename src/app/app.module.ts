817f4199c553d75a6ca2a0214b5620c4e3d45e8f
Denis Koliban <deniskoliban@gmail.com>
2021-08-11 14:39:03 +0300

    create preview component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { CreateHtmlComponent } from './create-html/create-html.component';
 import { CreateFormComponent } from './create-html/create-form/create-form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
+import { MaterialModule } from './app-modules/material.module';
+import { PreviewComponent } from './create-html/preview/preview.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CreateHtmlComponent,
-    CreateFormComponent
+    CreateFormComponent,
+    PreviewComponent
   ],
   imports: [
     BrowserModule,
