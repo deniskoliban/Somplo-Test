2365d2d1edf1c3513a5af97afd6b6908789c5318
Denis Koliban <deniskoliban@gmail.com>
2021-08-11 12:07:34 +0300

    create create-html component

---
+++ src/app/create-html/create-html.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-html',
+  templateUrl: './create-html.component.html',
+  styleUrls: ['./create-html.component.css']
+})
+export class CreateHtmlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
