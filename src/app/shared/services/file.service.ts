d86ff54b153918601be472bc6142751a2b600a6d
Denis Koliban <deniskoliban@gmail.com>
2021-08-12 12:37:40 +0300

    remove redundant type argument

--- src/app/shared/services/file.service.ts
+++ src/app/shared/services/file.service.ts
@@ -7,14 +7,7 @@ export class FileService {
 
   constructor() { }
 
-  dataURIToFile(dataURI: string, name: string, type: string): File {
-    const byteString = window.atob(dataURI);
-    const arrayBuffer = new ArrayBuffer(byteString.length);
-    const int8Array = new Uint8Array(arrayBuffer);
-    for (let i = 0; i < byteString.length; i++) {
-      int8Array[i] = byteString.charCodeAt(i);
-    }
-    const blob = new Blob([int8Array], { type: 'image/png' });
-    return new File([blob], name, { type })
+  blobToFile(blob: Blob, name: string): File {
+    return new File([blob], name, { type: blob.type })
   }
 }
