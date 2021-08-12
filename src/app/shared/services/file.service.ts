0cfb629295aa41b1deaa2c16e86cf83612fca38b
Denis Koliban <deniskoliban@gmail.com>
2021-08-12 11:31:49 +0300

    add dataURIToFile method to file service

--- src/app/shared/services/file.service.ts
+++ src/app/shared/services/file.service.ts
@@ -6,4 +6,15 @@ import { Injectable } from '@angular/core';
 export class FileService {
 
   constructor() { }
+
+  dataURIToFile(dataURI: string, name: string, type: string): File {
+    const byteString = window.atob(dataURI);
+    const arrayBuffer = new ArrayBuffer(byteString.length);
+    const int8Array = new Uint8Array(arrayBuffer);
+    for (let i = 0; i < byteString.length; i++) {
+      int8Array[i] = byteString.charCodeAt(i);
+    }
+    const blob = new Blob([int8Array], { type: 'image/png' });
+    return new File([blob], name, { type })
+  }
 }
