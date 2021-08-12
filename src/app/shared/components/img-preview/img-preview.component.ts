0af9790eb10e7875172643927e564897c3b30cdf
Denis Koliban <deniskoliban@gmail.com>
2021-08-12 10:25:24 +0300

    add img preview functionality

--- src/app/shared/components/img-preview/img-preview.component.ts
+++ src/app/shared/components/img-preview/img-preview.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-img-preview',
   templateUrl: './img-preview.component.html',
-  styleUrls: ['./img-preview.component.css']
+  styleUrls: ['./img-preview.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ImgPreviewComponent implements OnInit {
+  @Input() imageSrc = '';
 
   constructor() { }
 
